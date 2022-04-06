f7a0ca7a8ced035aa57c21e8ed9ba55fb8961aa7
Humblot Stéphane <stephane.humblot@etu.univ-grenoble-alpes.fr>
2022-04-06 10:19:38 +0200

    Update eslint - applied on web folder

--- web/ui.js
+++ web/ui.js
@@ -1,7 +1,9 @@
 (function () {
-  const Chessboard = chess.Chessboard;
-  const CaptureNotification = chess.notifications.CaptureNotification;
-  const MoveNotification = chess.notifications.MoveNotification;
+  // eslint-disable-next-line no-undef
+  const chessPkg = chess;
+  const Chessboard = chessPkg.Chessboard;
+  const CaptureNotification = chessPkg.notifications.CaptureNotification;
+  const MoveNotification = chessPkg.notifications.MoveNotification;
 
   /**
    * Dom element containing the messages to display to the history.
@@ -34,7 +36,7 @@
    * An object containing the icons of the pieces.
    * @type {Object.<number, HTMLImageElement>}
    */
-  let icons = {};
+  const icons = {};
 
   /**
    * The currently selected piece if any.
@@ -49,14 +51,14 @@
    * @param {number} file - the file in its number representation.
    * @return {string} returns the file in its string representation.
    */
-  let convertFileToString = function (file) {
+  const convertFileToString = function (file) {
     return String.fromCharCode(97 - 1 + file);
   };
 
   /**
    * Clear the history panel.
    */
-  let clearHistory = function () {
+  const clearHistory = function () {
     historyContainer.innerHTML = '';
   };
 
@@ -64,7 +66,7 @@
    * Add a message to the history panel.
    * @param {string} msg - the message to add.
    */
-  let addToHistory = function (msg) {
+  const addToHistory = function (msg) {
     historyContainer.innerHTML += `<p>${msg}</p>`;
   };
 
@@ -73,10 +75,10 @@
    * This function only clear the DOM element.
    * It does not affect the data model.
    */
-  let clearChessboard = function () {
+  const clearChessboard = function () {
     let icons = dom.querySelectorAll('img');
     for (let i = 0; i < icons.length; i++) {
-      let icon = icons[i];
+      const icon = icons[i];
       icon.remove();
     }
     icons = {};
@@ -88,8 +90,8 @@
    * Otherwise, update its position.
    * @param {Piece} piece - the piece graphically represented by the icon.
    */
-  let updateIcon = function (piece) {
-    let row = 8 - piece.rank,
+  const updateIcon = function (piece) {
+    const row = 8 - piece.rank,
       col = piece.file - 1;
 
     if (piece.isCaptured) {
@@ -97,7 +99,7 @@
       return;
     }
 
-    let rankElement = dom.querySelectorAll('.rank')[row],
+    const rankElement = dom.querySelectorAll('.rank')[row],
       fileElement = rankElement.children[col];
 
     fileElement.append(icons[piece.id]);
@@ -112,13 +114,13 @@
    * @param {Piece} piece - the piece that yields the notification.
    * @param {Notification} notification - the yield notification.
    */
-  let observe = function (piece, notification) {
+  const observe = function (piece, notification) {
     updateIcon(piece);
-    let color = piece.color,
+    const color = piece.color,
       name = piece.constructor.name;
 
     if (notification instanceof MoveNotification) {
-      let { oldPosition, newPosition } = notification;
+      const { oldPosition, newPosition } = notification;
 
       oldPosition.file = convertFileToString(oldPosition.file);
       newPosition.file = convertFileToString(newPosition.file);
@@ -128,7 +130,7 @@
       ${oldPosition.file}${oldPosition.rank} to
       ${newPosition.file}${newPosition.rank}`);
     } else if (notification instanceof CaptureNotification) {
-      let { pos } = notification;
+      const { pos } = notification;
 
       pos.file = convertFileToString(pos.file);
       addToHistory(`
@@ -140,10 +142,10 @@
   /**
    * Init the chessboard and prepare the different pieces..
    */
-  let init = function () {
+  const init = function () {
     chessboard.init();
     chessboard.pieces.forEach((p) => {
-      let img = new Image();
+      const img = new Image();
       img.src = `assets/img/icons/${p.constructor.name.toLowerCase()}_${
         p.color
       }.png`;
@@ -159,7 +161,7 @@
    * A reset function that successively clear the history panel,
    * clear the chesboard, and prepare a new game.
    */
-  let reset = function (e) {
+  const reset = function (e) {
     e.preventDefault();
     clearHistory();
     clearChessboard();
@@ -172,26 +174,26 @@
    * grab a piece, release a piece, or move/capture.
    * @param {MouseEvent} e - the mouse event.
    */
-  let handleClick = function (e) {
+  const handleClick = function (e) {
     e.preventDefault();
 
     // Retrieve the cell and its container.
     // The cell's container is the row representing the rank.
-    let cell = e.currentTarget,
+    const cell = e.currentTarget,
       container = cell.parentNode;
 
     // Retrieve the col and row numbers of the cell
-    let col = [...container.children].indexOf(cell),
+    const col = [...container.children].indexOf(cell),
       row = [...container.parentNode.children].indexOf(container);
 
     // Convert the col and row into the rank and file.
     // Row and col are 0-indexed
     // Rank and files are 1-indexed. Rank is in revesed order.
-    let rank = 8 - row,
+    const rank = 8 - row,
       file = col + 1;
 
     // Retrive a piece to a specific rank and file (if any).
-    let piece = chessboard.getPiece(rank, file);
+    const piece = chessboard.getPiece(rank, file);
 
     // Grab a piece
     if (!current && piece) {
@@ -208,7 +210,7 @@
       if (current.canMove(rank, file)) {
         // Before moving, remove the active state of the cell
         // where the piece currently lies in.
-        let oldRow = 8 - current.rank,
+        const oldRow = 8 - current.rank,
           oldCol = current.file - 1;
 
         rankElements[oldRow].children[oldCol].classList.remove('active');
